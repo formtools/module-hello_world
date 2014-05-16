aac1af538599d5bacbbda858c3e7db13e38feced
Ben Keen <ben.keen@gmail.com>
2014-05-15 21:30:22 -0700

    1.0.0

---
+++ index.tpl
@@ -0,0 +1,5 @@
+{include file='modules_header.tpl'}
+
+  <div class="title">{$L.phrase_hello_world|upper}</div>
+
+{include file='modules_footer.tpl'}
--- module.php
+++ module.php
@@ -7,9 +7,10 @@
 $MODULE["author"]          = "Encore Web Studios";
 $MODULE["author_email"]    = "formtools@encorewebstudios.com";
 $MODULE["author_link"]     = "http://modules.formtools.org";
-$MODULE["version"]         = "1.0.1";
-$MODULE["date"]            = "2011-05-26";
+$MODULE["version"]         = "1.0.0";
+$MODULE["date"]            = "2008-12-29";
 $MODULE["origin_language"] = "en_us";
+$MODULE["supports_ft_versions"] = "2.0.0";
 
 $MODULE["nav"] = array(
   "module_name" => array('index.php', false)
